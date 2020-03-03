80601e971839de982580a7a0555a568261e94c9c
Vojtech Horky <vojtech.horky@gmail.com>
2020-03-03 09:29:16 +0100

    Pylint

--- mff-gitlab.py
+++ mff-gitlab.py
@@ -4,20 +4,20 @@ import argparse
 import csv
 import locale
 import sys
-import d3s.gitlab as dg
-import gitlab
 import http
 import os
+import gitlab
+import d3s.gitlab as dg
 
 def load_users(path):
-    with open(path) as f:
-        data = csv.DictReader(f)
+    with open(path) as inp:
+        data = csv.DictReader(inp)
         return list(data)
 
-def as_gitlab_users(gl, users, login_column):
+def as_gitlab_users(glb, users, login_column):
     for user in users:
         user_login = user.get(login_column)
-        matching_users = gl.users.list(username=user_login)
+        matching_users = glb.users.list(username=user_login)
         if len(matching_users) == 0:
             print("WARNING: user {} not found!".format(user_login))
             continue
@@ -25,45 +25,47 @@ def as_gitlab_users(gl, users, login_column):
         user_obj.row = user
         yield user_obj
 
-def action_fork(gl, config):
+def action_fork(glb, config):
     users = load_users(config.csv_users)
-    
-    from_project = dg.get_canonical_project(gl, config.fork_from)
-    
-    for user in as_gitlab_users(gl, users, config.csv_users_login_column):
+
+    from_project = dg.get_canonical_project(glb, config.fork_from)
+
+    for user in as_gitlab_users(glb, users, config.csv_users_login_column):
         to_full_path = config.fork_to.format(**user.row)
         to_namespace = os.path.dirname(to_full_path)
         to_name = os.path.basename(to_full_path)
-        
-        print("Forking {} to {}/{} for user {}".format(from_project.path_with_namespace, to_namespace, to_name, user.username))
-        to_project = dg.fork_project_idempotent(gl, from_project, to_namespace, to_name)
-        
+
+        print("Forking {} to {}/{} for user {}".format(from_project.path_with_namespace,
+                                                       to_namespace, to_name,
+                                                       user.username))
+        to_project = dg.fork_project_idempotent(glb, from_project, to_namespace, to_name)
+
         if config.fork_hide_relationship:
-            dg.remove_fork_relationship(gl, to_project)
+            dg.remove_fork_relationship(glb, to_project)
 
 
-def action_unprotect_branch(gl, config):
+def action_unprotect_branch(glb, config):
     users = load_users(config.csv_users)
-    
-    for user in as_gitlab_users(gl, users, config.csv_users_login_column):
+
+    for user in as_gitlab_users(glb, users, config.csv_users_login_column):
         project_path = config.project_path.format(**user.row)
-        project = dg.get_canonical_project(gl, project_path)
-        
+        project = dg.get_canonical_project(glb, project_path)
+
         branch = project.branches.get(config.project_branch)
         print("Unprotecting branch {} on {}".format(branch.name, project.path_with_namespace))
         branch.unprotect()
 
-def action_add_member(gl, config):
+def action_add_member(glb, config):
     users = load_users(config.csv_users)
-        
+
     if config.access_level == 'devel':
         level = gitlab.DEVELOPER_ACCESS
     else:
         raise Exception("Unsupported access level.")
-    
-    for user in as_gitlab_users(gl, users, config.csv_users_login_column):
+
+    for user in as_gitlab_users(glb, users, config.csv_users_login_column):
         project_path = config.project_path.format(**user.row)
-        project = dg.get_canonical_project(gl, project_path)
+        project = dg.get_canonical_project(glb, project_path)
 
         try:
             print("Adding {} to {}".format(user.username, project.path_with_namespace))
@@ -71,11 +73,11 @@ def action_add_member(gl, config):
                 'user_id' : user.id,
                 'access_level' : level,
             })
-        except gitlab.GitlabCreateError as e:
-            if e.response_code == http.HTTPStatus.CONFLICT:
+        except gitlab.GitlabCreateError as exp:
+            if exp.response_code == http.HTTPStatus.CONFLICT:
                 pass
             else:
-                print(" -> error: {}".format(e))
+                print(" -> error: {}".format(exp))
 
 def main(argv):
     locale.setlocale(locale.LC_ALL, '')
@@ -94,7 +96,7 @@ def main(argv):
                              default=None,
                              dest='gitlab_instance',
                              help='Which GitLab instance to choose.')
-    
+
     args = argparse.ArgumentParser(description='MFF GitLab Wrapper')
     args.set_defaults(action='help')
     args_sub = args.add_subparsers(help='Select what to do')
@@ -125,7 +127,8 @@ def main(argv):
                            required=True,
                            dest='fork_to',
                            metavar='REPO_PATH_WITH_FORMAT',
-                           help='Target repository path, including formatting characters from CSV columns.')
+                           help='Target repository path, including ' \
+                                'formatting characters from CSV columns.')
     args_fork.add_argument('--hide-fork',
                            default=False,
                            dest='fork_hide_relationship',
@@ -150,7 +153,8 @@ def main(argv):
                                 required=True,
                                 dest='project_path',
                                 metavar='PROJECT_PATH_WITH_FORMAT',
-                                help='Project path, including formatting characters from CSV columns.')
+                                help='Project path, including ' \
+                                     'formatting characters from CSV columns.')
     args_unprotect.add_argument('--branch',
                                 required=True,
                                 dest='project_branch',
@@ -175,7 +179,8 @@ def main(argv):
                                  required=True,
                                  dest='project_path',
                                  metavar='PROJECT_PATH_WITH_FORMAT',
-                                 help='Project path, including formatting characters from CSV columns.')
+                                 help='Project path, including ' \
+                                      'formatting characters from CSV columns.')
     args_add_member.add_argument('--access-level',
                                  required=True,
                                  dest='access_level',
@@ -183,7 +188,7 @@ def main(argv):
                                  help='Currently only "devel" is recognized.')
 
     if len(argv) < 1:
-        class HelpConfig(object):
+        class HelpConfig:
             def __init__(self):
                 self.action = 'help'
         config = HelpConfig()
@@ -195,18 +200,18 @@ def main(argv):
         return
 
     if config.gitlab_config_file:
-        config.gitlab_config_file = [ config.gitlab_config_file ]
+        config.gitlab_config_file = [config.gitlab_config_file]
     else:
-        config.gitlab_config_file = [ '/etc/python-gitlab.cfg', '~/.python-gitlab.cfg' ]
+        config.gitlab_config_file = ['/etc/python-gitlab.cfg', '~/.python-gitlab.cfg']
 
-    gl = gitlab.Gitlab.from_config(config.gitlab_instance, config.gitlab_config_file)
-    
+    glb = gitlab.Gitlab.from_config(config.gitlab_instance, config.gitlab_config_file)
+
     if config.action == 'fork':
-        action_fork(gl, config)
+        action_fork(glb, config)
     elif config.action == 'unprotect':
-        action_unprotect_branch(gl, config)
+        action_unprotect_branch(glb, config)
     elif config.action == 'add-member':
-        action_add_member(gl, config)
+        action_add_member(glb, config)
     else:
         raise Exception("Unknown action.")
 
