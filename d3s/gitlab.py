4661c2c2dbf0825f2554feffad14d22c57c04709
Vojtech Horky <vojtech.horky@gmail.com>
2020-03-03 09:21:43 +0100

    Pylint

--- d3s/gitlab.py
+++ d3s/gitlab.py
@@ -1,17 +1,22 @@
+"""
+Helper GitLab functions.
+"""
 
-import gitlab
 import http
+import gitlab
 
-def get_canonical_project(gl, project):
-    if isinstance (project, ( int, str )):
-        return gl.projects.get (project)
-    if isinstance (project, gitlab.v4.objects.Project):
+
+def get_canonical_project(glb, project):
+    if isinstance(project, (int, str)):
+        return glb.projects.get(project)
+    if isinstance(project, gitlab.v4.objects.Project):
         return project
     raise Exception("Unexpected object type.")
 
-def fork_project_idempotent(gl, parent, fork_namespace, fork_name):
-    parent = get_canonical_project(gl, parent)
-    
+
+def fork_project_idempotent(glb, parent, fork_namespace, fork_name):
+    parent = get_canonical_project(glb, parent)
+
     try:
         fork_handle = parent.forks.create({
             'namespace' : fork_namespace,
@@ -19,21 +24,21 @@ def fork_project_idempotent(gl, parent, fork_namespace, fork_name):
             'name' : fork_name,
         })
         fork_identity = fork_handle.id
-    except gitlab.GitlabCreateError as e:
-        if e.response_code == http.HTTPStatus.CONFLICT:
+    except gitlab.GitlabCreateError as exp:
+        if exp.response_code == http.HTTPStatus.CONFLICT:
             fork_identity = "{}/{}".format(fork_namespace, fork_name)
         else:
             raise
 
-    return get_canonical_project(gl, fork_identity)
+    return get_canonical_project(glb, fork_identity)
 
-def remove_fork_relationship(gl, project):
-    project = get_canonical_project(gl, project)
+
+def remove_fork_relationship(glb, project):
+    project = get_canonical_project(glb, project)
     try:
         project.delete_fork_relation()
-    except gitlab.GitlabDeleteError as e:
-        if e.response_code == http.HTTPStatus.NOT_MODIFIED:
+    except gitlab.GitlabDeleteError as exp:
+        if exp.response_code == http.HTTPStatus.NOT_MODIFIED:
             pass
         else:
             raise
-
