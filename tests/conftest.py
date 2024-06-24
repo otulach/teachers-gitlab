d87a8b477860b44f6cbf135cf059be3178038472
Vojtech Horky <horky@d3s.mff.cuni.cz>
2024-06-24 16:57:53 +0200

    Code clean-up

--- tests/conftest.py
+++ tests/conftest.py
@@ -44,94 +44,15 @@ class MockedGitLabApi:
     def make_api_url_(self, suffix):
         return self.base_url + "api/v4/" + suffix
 
-    def escape_path(self, path_with_namespace):
-        import urllib.parse
-        return urllib.parse.quote_plus(path_with_namespace)
-
-    def quote_project_path_(self, path_with_namespace):
+    def escape_path_in_url(self, path_with_namespace):
         import urllib.parse
         return urllib.parse.quote_plus(path_with_namespace)
 
     def get_python_gitlab(self):
         return gitlab.Gitlab(self.base_url, oauth_token="mock_token")
 
-    def register_project(self, numerical_id, path_with_namespace, altering_responses=[]):
-        def get_and_register_again(original_self, my_url, req, remaining_responses):
-            self.logger.error("PROJECT: %s %s", req.method, req.url)
-
-            if remaining_responses:
-                extras = remaining_responses[0]
-                remaining_responses = remaining_responses[1:]
-            else:
-                extras = {}
-
-            base_info = {
-                "path_with_namespace": path_with_namespace,
-                "empty_repo": False,
-                "id": numerical_id,
-            }
-            body = json.dumps({**base_info, **extras})
-
-            original_self.responses.add_callback(
-                responses.GET,
-                my_url,
-                callback=lambda x: get_and_register_again(original_self, my_url, x, remaining_responses)
-            )
-
-            headers = {}
-            return (200, headers, body)
-
-        self.logger.info("Registered project %s %s", numerical_id, path_with_namespace)
-        for project_url in [self.make_api_url_("projects/" + self.quote_project_path_(path_with_namespace)), self.make_api_url_(f"projects/{numerical_id}")]:
-            self.logger.info(" -> %s", project_url)
-            self.responses.add_callback(
-                responses.GET,
-                project_url,
-                callback=lambda x: get_and_register_again(self, project_url, x, altering_responses),
-                content_type="application/json",
-            )
-
-
-    def add_project(self, numerical_id, path_with_namespace):
-        import urllib.parse
-
-        def get_project_info(req):
-            self.logger.info("PROJECT: %s %s", req.method, req.url)
-            body = json.dumps({
-                "path_with_namespace": path_with_namespace,
-                "empty_repo": False,
-                "id": numerical_id,
-            })
-            headers = {}
-            return (201, headers, body)
-
-        # FIXME: for some calls it is okay that they are called multiple-times
-        for _ in [1, 2, 3, 4, 5, 6]:
-            self.responses.add_callback(
-                responses.GET,
-                "http://localhost/api/v4/projects/" + urllib.parse.quote_plus(path_with_namespace),
-                callback=get_project_info,
-                content_type="application/json",
-            )
-        self.responses.add_callback(
-            responses.GET,
-            "http://localhost/api/v4/projects/" + str(numerical_id),
-            callback=get_project_info,
-            content_type="application/json",
-        )
-
-    def on_api_get(self, url, *args, **kwargs):
-        return self.responses.get(self.base_url + "api/v4/" + url, *args, **kwargs)
-
-    def on_api_post(self, url, *args, **kwargs):
-        return self.responses.post(self.base_url + "api/v4/" + url, *args, **kwargs)
-
-    def logging_callback_200(self, req, body):
-        self.logger.info("URL matched: %s %s", req.method, req.url)
-        return (200, {}, body)
-
-    def api_get(self, url, response_json, helper=False, *args, **kwargs):
-        full_url = self.base_url + "api/v4/" + url
+    def on_api_get(self, url, response_json, helper=False, *args, **kwargs):
+        full_url = self.make_api_url_(url)
 
         kwargs['json'] = response_json
 
@@ -143,9 +64,7 @@ class MockedGitLabApi:
             result._calls.add_call(None)
 
 
-    def api_post(self, url, request_json, response_json, *args, **kwargs):
-        full_url = self.base_url + "api/v4/" + url
-
+    def on_api_post(self, url, request_json, response_json, *args, **kwargs):
         kwargs['body'] = json.dumps(response_json)
         kwargs['match'] = [
             responses.matchers.json_params_matcher(request_json)
@@ -153,7 +72,7 @@ class MockedGitLabApi:
         kwargs['content_type'] = 'application/json'
 
         return self.responses.post(
-            full_url,
+            self.make_api_url_(url),
             *args,
             **kwargs,
         )
--- tests/test_experimenting.py
+++ tests/test_experimenting.py
@@ -23,8 +23,8 @@ class MockEntries:
             yield entry, None
 
 def test_fork_one(mock_gitlab):
-    mock_gitlab.api_get(
-        'projects/' + mock_gitlab.escape_path('base/repo'),
+    mock_gitlab.on_api_get(
+        'projects/' + mock_gitlab.escape_path_in_url('base/repo'),
         response_json={
             'id': 42,
             'path_with_namespace': 'base/repo'
@@ -32,7 +32,7 @@ def test_fork_one(mock_gitlab):
         helper=True,
     )
 
-    mock_gitlab.api_post(
+    mock_gitlab.on_api_post(
         'projects/42/fork',
         request_json={
             'name': 'alpha',
@@ -44,7 +44,7 @@ def test_fork_one(mock_gitlab):
         }
     )
 
-    mock_gitlab.api_get(
+    mock_gitlab.on_api_get(
         'projects/17',
         response_json={
             'id': 17,
@@ -52,16 +52,16 @@ def test_fork_one(mock_gitlab):
             'empty_repo': True,
         },
     )
-    mock_gitlab.api_get(
-        'projects/' + mock_gitlab.escape_path('student/alpha'),
+    mock_gitlab.on_api_get(
+        'projects/' + mock_gitlab.escape_path_in_url('student/alpha'),
         response_json={
             'id': 17,
             'path_with_namespace': 'student/alpha',
             'empty_repo': True,
         },
     )
-    mock_gitlab.api_get(
-        'projects/' + mock_gitlab.escape_path('student/alpha'),
+    mock_gitlab.on_api_get(
+        'projects/' + mock_gitlab.escape_path_in_url('student/alpha'),
         response_json={
             'id': 17,
             'path_with_namespace': 'student/alpha',
@@ -75,54 +75,6 @@ def test_fork_one(mock_gitlab):
         mock_gitlab.get_python_gitlab(),
         logging.getLogger("fork"),
         MockEntries([{'login': 'alpha'}]),
-        'login',
-        'base/repo',
-        'student/{login}',
-        False,
-        True
-    )
-
-
-def x_test_fork_another(mock_gitlab):
-    mock_gitlab.register_project(15, "base/repo")
-    mock_gitlab.register_project(16, "student/a", altering_responses=[
-        {"empty_repo": True},
-        {}
-    ])
-    mock_gitlab.register_project(17, "student/b", altering_responses=[
-        {"empty_repo": True},
-        {}
-    ])
-
-    #mock_gitlab.on_api_post("projects/15/fork", json={"id": 17}, status=200)
-
-    mock_gitlab.report_unknown()
-
-    tg.action_fork(
-        mock_gitlab.get_python_gitlab(),
-        logging.getLogger("fork"),
-        MockEntries([{'login': 'a'}, {'login': 'b'}]),
-        'login',
-        'base/repo',
-        'student/{login}',
-        False,
-        True
-    )
-
-def xxy_test_fork_incomplete(mock_gitlab):
-    mock_gitlab.add_project(15, "base/repo")
-    mock_gitlab.add_project(16, "student/a")
-    mock_gitlab.add_project(17, "student/b")
-
-    #mock_gitlab.on_api_post("projects/15/fork", json={"id": 16}, status=200)
-    mock_gitlab.on_api_post("projects/15/fork", json={"id": 17}, status=200)
-
-    mock_gitlab.report_unknown()
-
-    tg.action_fork(
-        mock_gitlab.get_python_gitlab(),
-        logging.getLogger("fork"),
-        MockEntries([{'login': 'a'}, {'login': 'b'}]),
         'login',
         'base/repo',
         'student/{login}',
