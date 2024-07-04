68ecb1e1891b51b4343e22b5373a44ced33e96dc
Ondřej Tulach <ondrej.tulach@gmail.com>
2024-07-04 09:09:24 +0200

    Added tests for actions create-tag and protect-tag.

--- tests/test_createtag.py
+++ tests/test_createtag.py
@@ -30,3 +30,33 @@ def test_create_tag(mock_gitlab):
         '',
         ''
     )
+    
+def test_create_existing_tag(mock_gitlab):
+    entries = [
+        {'login': 'alpha'},
+    ]
+    
+    mock_gitlab.register_project(452, 'student/alpha')
+    
+    mock_gitlab.on_api_post(
+        'projects/452/repository/tags',
+        request_json={
+            'ref': 'double', 
+            'tag_name': 'tag2'
+            },
+        response_json={
+            "message": "already exists"
+        }
+    )
+
+    mock_gitlab.report_unknown()
+
+    tg.action_create_tag(
+        mock_gitlab.get_python_gitlab(),
+        logging.getLogger("createtag"),
+        tg.ActionEntries(entries),
+        'student/{login}',
+        'tag2',
+        'double',
+        ''
+    )   
