0c80fb67f690ead046ffacd442ae6fd6384fc463
Ondřej Tulach <ondrej.tulach@gmail.com>
2024-07-08 23:45:14 +0200

    Issues with create_access_levels in testing action protect-tag

--- tests/test_protecttag.py
+++ tests/test_protecttag.py
@@ -2,7 +2,7 @@ import logging
 
 import teachers_gitlab.main as tg
 
-def test_protect_tag(mock_gitlab):
+def test_protect_tag_without_response(mock_gitlab):
     entries = [
         {'login': 'alpha'},
     ]
@@ -34,4 +34,41 @@ def test_protect_tag(mock_gitlab):
         'tag1',
         'devel'
     )
+    
+def test_protect_tag(mock_gitlab):
+    entries = [
+        {'login': 'alpha'},
+    ]
+    
+    mock_gitlab.register_project(452, 'student/alpha')
+    
+    mock_gitlab.on_api_get(
+        'projects/452/protected_tags/tag1',
+        response_json={
+            'id': 1,
+            'name': 'tag1'
+        },
+    )
+    
+    mock_gitlab.on_api_post(
+        'projects/452/protected_tags',
+        request_json={
+            'name': 'tag1',
+            'create_access_level':'developers-can-create'
+            },
+        response_json={
+        }
+    )
+    
+    mock_gitlab.report_unknown()
+    
+    tg.action_protect_tag(
+        mock_gitlab.get_python_gitlab(),
+        logging.getLogger("protecttag"),
+        tg.ActionEntries(entries),
+        'student/{login}',
+        'tag1',
+        'developers-can-create'
+    )
+      
     
